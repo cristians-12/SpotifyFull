db01d8e27b5383dbc75c109b09ab9e0815cdbecf
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-10-07 14:31:45 -0500

    side bar fix

--- spotify/src/app/components/SideBar.tsx
+++ spotify/src/app/components/SideBar.tsx
@@ -1,6 +1,6 @@
 export default function SideBar(){
     return(
-        <div className="bg-[#121212] hidden lg:block w-[30vw] rounded-lg h-screen">
+        <div className="bg-[#121212] hidden lg:block w-[25vw] rounded-lg h-screen">
             <div className="text-gray-500 font-bold text-center p-5">
                 Tu biblioteca
             </div>
