38a0b6718c22f3a697af5ab80a560ec4872e3f8a
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-12-09 15:23:06 -0500

    authstore setloged

--- frontend/src/store/authStore.ts
+++ frontend/src/store/authStore.ts
@@ -2,10 +2,12 @@ import { create } from "zustand";
 
 type Auth = {
   isLoggedIn: boolean;
+  setLoggedin: (value: boolean) => void;
 };
 
 const useAuthStore = create<Auth>((set) => ({
   isLoggedIn: false,
+  setLoggedin: (value: boolean) => set(() => ({ isLoggedIn: value })),
 }));
 
 export default useAuthStore;
